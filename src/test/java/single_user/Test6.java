bd0a9cf946a6b858aa93cb089998b670d661172e
student <student@192.168.1.36>
2020-12-28 21:47:59 +0800

    springdata

--- src/test/java/single_user/Test6.java
+++ src/test/java/single_user/Test6.java
@@ -9,6 +9,8 @@ import org.junit.Test;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.domain.Sort.Order;
 
 public class Test6 {
     @Test
@@ -18,8 +20,12 @@ public class Test6 {
         //分頁
         int pageNo = 0;
         int pageSize = 10;
+        //排序
+        Order order1 = new Order(Sort.Direction.ASC, "name");
+        Order order2 = new Order(Sort.Direction.DESC, "id");
+        Sort sort = new Sort(order1, order2);
         //分頁請求
-        PageRequest pageRequest = new PageRequest(pageNo, pageSize);
+        PageRequest pageRequest = new PageRequest(pageNo, pageSize, sort);
         Page<UserView> page = uvr.findAll(pageRequest);
         System.out.println(page.getContent());//得到分頁內容
         System.out.println((pageNo+1) + "/" + page.getTotalPages());
